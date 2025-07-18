4ccdf7273f78997428968d71de62f8d6edd4efed
unknown <abdourahmane.n.ngom@gmail.com>
2025-07-18 09:35:09 +0200

    Setup de l'application FastAPI

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Path
+from uuid import uuid4
+from typing import List, Dict
 
 app = FastAPI(title= "Calculateur RPN")
 
@@ -7,41 +9,36 @@ piles = dict()
 operateurs = {"+","-"
               "*","/"}
 
-def creer_pile():
-    pass
-
+@app.get("/rpn/op", summary="Liste des opérateurs")
 def lister_piles():
+    return {"operateurs": list(operateurs)}
+
+@app.post("/rpn/stack", summary="Créer une nouvelle pile")
+def creer_pile():
+    pile_id = str(uuid4())
+    piles[pile_id] = []
+    return {"pile_id": pile_id, "pile": piles[pile_id]}
+
+@app.get("/rpn/stack", summary="Lister toutes les piles")
+def recuperer_toutes_les_piles():
+    return {"piles": list(piles.keys())}
+
+@app.get("/rpn/stack/{pile_id}", summary="Voir une pile")
+def recuperer_une_piles():
     pass
 
-def voir_pile():
-    pass
-
-
+@app.delete("/rpn/stack/{pile_id}", summary="Vider une pile")
 def nettoyer_une_pile():
     pass
 
+@app.post("/rpn/stack/{pile_id}", summary="Ajouter une valeur à la pile")
 def ajouter_element_dans_pile():
     pass
 
 
-def recuperer_toutes_les_piles():
-    pass
-
+@app.post("/rpn/op/{operateur}/stack/{pile_id}", summary="Appliquer un opérateur")
 def appliquer_operateur():
     pass
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
