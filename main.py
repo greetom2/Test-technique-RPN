2ee923990334380339a61deac375be3537d81989
unknown <abdourahmane.n.ngom@gmail.com>
2025-07-18 10:33:14 +0200

    Typage varibale et fonctions, test Swagger + correction bugs sur fonction nettoyer_une_pile

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from uuid import uuid4
-from typing import List, Dict
+from typing import List, Dict, Any
 import webbrowser
 import uvicorn
 
@@ -9,7 +9,7 @@ app = FastAPI(title= "Calculateur RPN")
 
 piles: Dict[str, List[int]] = {}
 
-operateurs = {"+","-"
+operateurs: set[str] = {"+","-"
               "*","/"}
 
 class Valeur(BaseModel):
@@ -20,30 +20,30 @@ def lister_piles():
     return {"operateurs": list(operateurs)}
 
 @app.post("/rpn/stack", summary="Créer une nouvelle pile")
-def creer_pile():
+def creer_pile() -> Dict[str, Any]:
     pile_id = str(uuid4())
     piles[pile_id] = []
     return {"pile_id": pile_id, "pile": piles[pile_id]}
 
 @app.get("/rpn/stack", summary="Lister toutes les piles")
-def recuperer_toutes_les_piles():
+def recuperer_toutes_les_piles() -> Dict[str, List[str]]:
     return {"piles": list(piles.keys())}
 
 @app.get("/rpn/stack/{pile_id}", summary="Voir une pile")
-def recuperer_une_pile(pile_id: str ):
+def recuperer_une_pile(pile_id: str ) -> Dict[str, List[int]]:
     if pile_id not in piles:
         raise HTTPException(status_code=404, detail="Pile introuvable")
     return {"pile": piles[pile_id]}
 
 @app.delete("/rpn/stack/{pile_id}", summary="Vider une pile")
-def nettoyer_une_pile(pile_id: str):
-        if pile_id not in piles:
-            raise HTTPException(status_code=404, detail="Pile introuvable")
-        piles[pile_id] = []
-        return {"message": "Pile vidée", "pile": piles[pile_id]}
+def nettoyer_une_pile(pile_id: str) -> Dict[str, str]:
+    if pile_id not in piles:
+        raise HTTPException(status_code=404, detail="Pile introuvable")
+    del piles[pile_id]
+    return {"message": "Pile supprimée"}
 
 @app.post("/rpn/stack/{pile_id}", summary="Ajouter une valeur à la pile")
-def ajouter_element_dans_pile(pile_id: str, val: Valeur):
+def ajouter_element_dans_pile(pile_id: str, val: Valeur) -> Dict[str, List[int]]:
             if pile_id not in piles:
                 raise HTTPException(status_code=404, detail="Pile introuvable")
             piles[pile_id].append(val.value)
@@ -51,7 +51,7 @@ def ajouter_element_dans_pile(pile_id: str, val: Valeur):
 
 
 @app.post("/rpn/op/{operateur}/stack/{pile_id}", summary="Appliquer un opérateur")
-def appliquer_operateur(op: str, pile_id: str):    
+def appliquer_operateur(op: str, pile_id: str) -> Dict[str, List[Any]]:    
         if op not in operateurs:
             raise HTTPException(status_code=400, detail="Opérateur invalide")
         if pile_id not in piles:
